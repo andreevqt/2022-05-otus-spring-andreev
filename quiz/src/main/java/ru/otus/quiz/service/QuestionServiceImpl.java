415ffebb8ac91398f6ab4b2d1f664cb8f5ee5b86
qprquo <qprquo@gmail.com>
2022-06-05 02:07:16 +0300

    refactor(quiz): remove unused methods

--- quiz/src/main/java/ru/otus/quiz/service/QuestionServiceImpl.java
+++ quiz/src/main/java/ru/otus/quiz/service/QuestionServiceImpl.java
@@ -5,38 +5,19 @@ import org.springframework.stereotype.Service;
 import ru.otus.quiz.dao.QuestionDao;
 import ru.otus.quiz.domain.Question;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class QuestionServiceImpl implements QuestionService {
 
   private final QuestionDao questionDao;
-  private final Map<Integer, Boolean> answers;
 
   @Autowired
   public QuestionServiceImpl(QuestionDao questionDao) {
     this.questionDao = questionDao;
-    this.answers = new HashMap<>();
-  }
-
-  public void answer(int questionId, int answer) {
-    var questions = questionDao.findAll();
-    questions.stream()
-      .filter(q -> q.getId() == questionId)
-      .findFirst()
-      .ifPresent(question -> answers.put(
-        questionId,
-        question.getCorrectAnswer() == answer
-      ));
   }
 
   public List<Question> listAll() {
     return questionDao.findAll();
-  }
-
-  public int getScore() {
-    return answers.values().stream().reduce(0, (acc, value) -> acc + (value ? 1 : 0), Integer::sum);
   }
 }
