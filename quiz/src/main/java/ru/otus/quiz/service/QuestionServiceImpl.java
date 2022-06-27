89472b4ee7d53abe7a5c95602dd067174b02bd5d
qprquo <qprquo@gmail.com>
2022-06-27 11:42:45 +0300

    add allargs annotation

--- quiz/src/main/java/ru/otus/quiz/service/QuestionServiceImpl.java
+++ quiz/src/main/java/ru/otus/quiz/service/QuestionServiceImpl.java
@@ -1,5 +1,6 @@
 package ru.otus.quiz.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.otus.quiz.dao.QuestionDao;
@@ -8,13 +9,10 @@ import ru.otus.quiz.domain.Question;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class QuestionServiceImpl implements QuestionService {
   private final QuestionDao questionDao;
 
-  @Autowired
-  public QuestionServiceImpl(QuestionDao questionDao) {
-    this.questionDao = questionDao;
-  }
   public List<Question> listAll() {
     return questionDao.findAll();
   }
