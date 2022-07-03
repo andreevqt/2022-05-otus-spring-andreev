d907734abbacc22b4d41868cfcd70ff19260a60a
qprquo <qprquo@gmail.com>
2022-07-04 00:06:58 +0300

    add yaml properties

--- spring-boot-quiz/src/main/java/ru/otus/springbootquiz/config/AppConfig.java
+++ spring-boot-quiz/src/main/java/ru/otus/springbootquiz/config/AppConfig.java
@@ -1,16 +1,17 @@
 package ru.otus.springbootquiz.config;
 
+import lombok.AllArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@ConfigurationProperties("application")
+@Setter
 @Component
 public class AppConfig implements QuestionsResourceProvider {
 
-  private final String questionsResource;
-
-  public AppConfig(@Value("${questions.resource}") String questionsResource) {
-    this.questionsResource = questionsResource;
-  }
+  private String questionsResource;
 
   @Override
   public String getQuestionsResource() {
