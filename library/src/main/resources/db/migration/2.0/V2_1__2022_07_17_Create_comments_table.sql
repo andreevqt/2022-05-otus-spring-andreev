6e4b6e8be064b856f62f8ad1f990be972c9e1f15
Alex <qprquo@gmail.com>
2022-07-20 15:22:35 +0300

    Fix migration

--- library/src/main/resources/db/migration/2.0/V2_1__2022_07_17_Create_comments_table.sql
+++ library/src/main/resources/db/migration/2.0/V2_1__2022_07_17_Create_comments_table.sql
@@ -1,6 +1,6 @@
 create table comments
 (
-    id      bigserial,
+    id      bigserial primary key,
     book_id bigint,
     content text,
     foreign key (book_id) REFERENCES books (id) on delete cascade
